87c59ed5bf9492254df81b7e9d8ad7a12b8a68a3
Bryan Ulises Gómez <46952551+UlisesGomezDW@users.noreply.github.com>
2021-07-02 17:47:56 -0500

    summary prettier

--- src/grapql.config.js
+++ src/grapql.config.js
@@ -1,5 +1,5 @@
-import ApolloClient from "apollo-boost";
+import ApolloClient from "apollo-boost"
 
 export const client = new ApolloClient({
-  uri: "https://rickandmortyapi.com/graphql"
-});
+    uri: "https://rickandmortyapi.com/graphql",
+})
--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { ApolloProvider } from "react-apollo";
-import { client } from "./grapql.config";
-import App from "./AppV2";
+import React from "react"
+import ReactDOM from "react-dom"
+import { ApolloProvider } from "react-apollo"
+import { client } from "./grapql.config"
+import App from "./AppV2"
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById("root")
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
-  rootElement
-);
+    <ApolloProvider client={client}>
+        <App />
+    </ApolloProvider>,
+    rootElement
+)
